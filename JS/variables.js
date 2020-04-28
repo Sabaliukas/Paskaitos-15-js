80601e5f867b6f935a8ec8663dc0bfd244ee6a38
Sabaliukas <63238879+Sabaliukas@users.noreply.github.com>
2020-04-28 13:45:26 +0300

    Kintamuju iniciavimas

---
+++ JS/variables.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*
+kitamuju tipai:
+-const
+-let
+-var
+*/
+
+const manoVardas = 'Audrius';              // tekstas
+const gimimoMetai = 1987;                   // skaicius
+const gimimoMenesis = 'Lapkritis'           // tekstas
+const gimesKaune = true;                    // logine reiksme: true / false
+const pazymiai = []                         // tuscias sarasas
+const pazymiaiMarytes = [10, 10, 2, 8,]     // sarasas su skaiciais
+const abc = ['a', 'b', 'c', 'd',];          // sarasas su tekstais
+
+
+
+const metai = 1987;
+const menesis = 11;
+const diena = 25;
+
+console.log(metai)
+console.log(menesis)
+console.log(diena)
+
+const vardas = 'Audrius';
+const pavarde = 'Sabaliauskas';
+const miestas = 'Kaunas';
+
+console.log(vardas)
+console.log(pavarde)
+console.log(miestas)
+
+const svaitesDienos = [1, 2, 3, 4, 5,]
+const ziemosMenesiai = [11, 12, 1, 2,]
+const pavasarioMenesiai = [3, 4, 5, 6,]
+
+console.log(svaitesDienos)
+console.log(ziemosMenesiai)
+console.log(pavasarioMenesiai)
+
+const prisistatymas = 'Sveiki as esu audrius'
+const pavasaris = 'kovas balandis geguze'
+const ziema = 'lapkritis gruodis sausis'
+
+console.log(pavasaris)
+console.log(ziema)
+console.log(prisistatymas)
